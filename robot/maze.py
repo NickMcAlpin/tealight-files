2ceb4f68bc55b64fbf76aa65b6059951a0723cd5
NickMcAlpin <nickmcalpin@yahoo.co.uk>
2014-08-19 14:41:52 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -6,4 +6,7 @@ from tealight.robot import (move,
                             left_side, 
                             right_side)
 
-# Add your code here
+if touch() == 'wall':
+  if left_side() != 'wall':
+    
+    
