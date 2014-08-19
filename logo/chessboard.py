b62b5e63f4c0b2030e160a4834edf20f7c4a1c7a
NickMcAlpin <nickmcalpin@yahoo.co.uk>
2014-08-19 12:20:56 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -6,13 +6,7 @@ def w_square():
     turn(90)
     color("black")
   
-for x in range(0,8):
-  for i in range(0,8):
-    w_square()
-    move(25)
-  turn(90)
-  move(25)
-  turn(90)
-  move(200)
-  turn(90)
-  
+# for x in range (0,8):
+for i in range(0,8):
+  w_square()
+  move(25)  
