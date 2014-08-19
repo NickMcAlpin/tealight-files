412379fa60c716cd1cfa0746091411400830cb5d
NickMcAlpin <nickmcalpin@yahoo.co.uk>
2014-08-19 12:21:39 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -6,7 +6,7 @@ def w_square():
     turn(90)
     color("black")
   
-# for x in range (0,8):
-for i in range(0,8):
-  w_square()
-  move(25)  
+for x in range (0,8):
+  for i in range(0,8):
+    w_square()
+    move(25)  
