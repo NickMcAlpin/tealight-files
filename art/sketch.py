d866ad0a5ef0b9a5f88937c217644e2c42ea7353
NickMcAlpin <nickmcalpin@yahoo.co.uk>
2014-08-20 10:35:31 +0100

    Running art/sketch.py

--- art/sketch.py
+++ art/sketch.py
@@ -3,6 +3,8 @@ from tealight.art import (color, line, spot, circle, box, image, text, backgroun
 lastx = 0
 lasty = 0
 
+colour = "blue"
+
 def handle_mousedown(x,y):
   global lastx, lasty
   
@@ -13,12 +15,10 @@ def handle_mousemove(x,y,button):
   global lastx, lasty
   
   if button == "left":
-    colour = "blue"
     line(lastx, lasty, x, y)
     lastx = x
     lasty = y
   if button == "right":
-    colour = "black"
     line(lastx, lasty, x, y)
     lastx = x
     lasty = y
