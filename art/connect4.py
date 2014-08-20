7fecabf4ab9b4874eac82da0a7b9ec1121dde430
NickMcAlpin <nickmcalpin@yahoo.co.uk>
2014-08-20 12:23:46 +0100

    Running art/connect4.py

--- art/connect4.py
+++ art/connect4.py
@@ -1,18 +1,3 @@
-from tealight.logo import (move, turn, color)
+from tealight.art import rectangle
 
-def c_square():
-  for i in range(0,4):
-    move(25)
-    turn(90)
-    color("black")
-  
-  
-for x in range (0,8):
-  for i in range(0,8):
-    c_square()
-    move(25)
-  turn(90)
-  move(25)
-  turn(90)
-  move(200)
-  turn(180)
+rectangle
