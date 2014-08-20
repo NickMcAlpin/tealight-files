1d8ecdb52df1bde77928b36f93db8723d8a9fc48
NickMcAlpin <nickmcalpin@yahoo.co.uk>
2014-08-20 12:10:04 +0100

    Closing art/connect4.py

--- art/connect4.py
+++ art/connect4.py
@@ -1,1 +1,26 @@
-print "A new file!"
+from tealight.logo import (move, turn, color)
+
+# Creates chessboard
+
+def c_wsquare():
+  for i in range(0,4):
+    move(25)
+    turn(90)
+    color("black")
+    
+def c_bsquare():
+  for i in range(0,4):
+    move(25)
+    turn(90)
+    color("black")
+  
+  
+for x in range (0,8):
+  for i in range(0,8):
+    c_wsquare()
+    move(25)
+  turn(90)
+  move(25)
+  turn(90)
+  move(200)
+  turn(180)
