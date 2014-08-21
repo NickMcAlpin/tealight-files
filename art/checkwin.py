d91bbd55afd834acdcc109ecee7bce26049064f3
NickMcAlpin <nickmcalpin@yahoo.co.uk>
2014-08-21 12:27:25 +0100

    Running art/checkwin.py

--- art/checkwin.py
+++ art/checkwin.py
@@ -1,5 +1,3 @@
-global matrix
-
 def initialiseMatrix():
   matrix = [[0 for i in range(7)] for j in range(7)]
   
@@ -12,37 +10,6 @@ def initialiseMatrix():
 
 matrix = initialiseMatrix()
 
-#check diagonal from left to right
-def checkDiagonalLR():
-  for row in range(0,6):
-    for col in range (0,6):
-      if matrix[row][col] == 1 and matrix[row][col] == matrix[row+1][col+1] and matrix[row][col] == matrix[row+2][col+2] and matrix[row][col] == matrix[row+3][col+3]:
-        print("four in a row")
-      
-
-#check diagonal from right to left
-def checkDiagonalRL():
-  for row in range(0,6):
-    for col in range (0,6):
-      if matrix[row][col] == 1 and matrix[row][col] == matrix[row-1][col+1] and matrix[row][col] == matrix[row-2][col+2] and matrix[row][col] == matrix[row-3][col+3]:
-        return 1
-      else:
-        return 0
-
-#check horizontal 
-def checkHorizontal():
-  for row in range(0,6):
-    for col in range (0,6):
-      if matrix[row][col] == 1 and matrix[row][col] == matrix[row+1][col] and matrix[row][col] == matrix[row+2][col] and matrix[row][col] == matrix[row+3][col]:
-        print("four in a row")
-
-#check vertical
-def checkVertical():
-  for row in range(0,6):
-    for col in range (0,6):
-      if matrix[row][col] == 1 and matrix[row][col] == matrix[row][col-1] and matrix[row][col] == matrix[row][col-2] and matrix[row][col] == matrix[row][col-3]:
-        print("four in a row")
-
 def checkwin():
   for row in range(0,6):
     for col in range (0,6):
@@ -54,5 +21,3 @@ def checkwin():
         
       else:
         return 0
-      
-print(checkwin())
