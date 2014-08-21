827ec8bcf71dc9a129bc0e33066b852adfe1629b
NickMcAlpin <nickmcalpin@yahoo.co.uk>
2014-08-21 10:41:54 +0100

    Closing art/connect4.py

--- art/connect4.py
+++
@@ -1,9 +0,0 @@
-from tealight.art import *
-
-def rectangle(x, y, width, height):
-  line(x, y, width, y)
-  line(x, height, width, height)
-  line(width, y, width, height)
-  line(x, y, x, height)
-  
-rectangle(100,100,100,100)
