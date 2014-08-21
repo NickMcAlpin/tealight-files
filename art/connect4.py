8b3f0938b9e109daba1df8ff473a3d5e5e080eed
NickMcAlpin <nickmcalpin@yahoo.co.uk>
2014-08-21 10:39:29 +0100

    Running art/connect4.py

--- art/connect4.py
+++ art/connect4.py
@@ -3,3 +3,7 @@ from tealight.art import *
 def rectangle(x, y, width, height):
   line(x, y, width, y)
   line(x, height, width, height)
+  line(width, y, width, height)
+  line(x, y, x, height)
+  
+rectangle(20,20,20,20)
