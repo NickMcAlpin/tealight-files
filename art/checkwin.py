29d54995780ed05cceb6defc657740cf0ff48f54
NickMcAlpin <nickmcalpin@yahoo.co.uk>
2014-08-21 11:31:39 +0100

    Running art/checkwin.py

--- art/checkwin.py
+++ art/checkwin.py
@@ -10,6 +10,7 @@ def initialiseMatrix():
   print(matrix)
   print(len(matrix))
   return matrix
+  print(true)
 
 
 matrix = initialiseMatrix()
@@ -26,7 +27,9 @@ def checkDiagonalRL():
   for row in range(0,6):
     for col in range (0,6):
       if matrix[row][col] == 1 and matrix[row][col] == matrix[row-1][col+1] and matrix[row][col] == matrix[row-2][col+2] and matrix[row][col] == matrix[row-3][col+3]:
-        print("four in a row")
+        return 1
+      else:
+        return 0
 
 #check horizontal 
 def checkHorizontal():
